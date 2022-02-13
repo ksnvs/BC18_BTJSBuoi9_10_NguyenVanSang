cae5b5ce0bfe6f2785aa73bf3da6479cdf1e587b
SangNguyen <SangNguyen>
2022-02-13 21:48:02 +0700

    Update source code

--- js/QLNV.js
+++ js/QLNV.js
@@ -13,6 +13,13 @@ if (dsNVLocal !== null) {
   renderTable(dsNV);
 }
 // Loading data from local Storage
+
+// Save local Storage
+function luuDataLocal() {
+  var dsNVjson = JSON.stringify(dsNV);
+  localStorage.setItem("dsNVLocal", dsNVjson);
+}
+// Save local Storage
 function clearAllFields() {
   document.getElementById("txtMaNV").value = "";
   document.getElementById("txtTenNV").value = "";
@@ -29,6 +36,15 @@ function clearAllFields() {
 function SelectChangeVal() {
   isEmpty("txtChucVuNV", "spanCVNV");
 }
+
+function xoaNV(_maNV) {
+  var index = timKiemNV(_maNV);
+  dsNV.splice(index, 1);
+  renderTable(dsNV);
+
+  luuDataLocal();
+}
+
 function themNV() {
   var maNV = document.getElementById("txtMaNV").value;
   var tenNV = document.getElementById("txtTenNV").value;
@@ -50,8 +66,51 @@ function themNV() {
   ) {
     return;
   }
-
+  var maIsValid = false;
+  dsNV.map(function (nv) {
+    console.log(nv);
+    if (nv.Ma === maNV) {
+      maIsValid = true;
+    } else {
+      maIsValid = false;
+    }
+  });
+  if (maIsValid) {
+    alert("Mã nhân viên đã tồn tại");
+    return;
+  }
   var NV = new NhanVien(maNV, tenNV, emailNV, ngaySinhNV, chucVuNV);
   dsNV.push(NV);
-  console.log({ NV });
+  renderTable(dsNV);
+}
+
+function renderTable(array) {
+  var contentHTML = "";
+  for (var index = 0; index < array.length; index++) {
+    var nv = array[index];
+    contentHTML += `
+    <tr>
+    <td>${nv.Ma}</td>
+    <td>${nv.Ten}</td>
+    <td>${nv.Email}</td>
+    <td>${nv.NgaySinh}</td>    
+    <td>${nv.ChucVu}</td> 
+    <td>
+    <button onclick="suaNV(${nv.Ma})" class="btn btn-success">Sửa</button>
+    <button onclick="xoaNV(${nv.Ma})" class="btn btn-danger">Xóa</button>
+    </td>
+    </tr>
+    `;
+  }
+  document.getElementById("tbodyNhanVien").innerHTML = contentHTML;
+
+  luuDataLocal();
+}
+
+function timKiemNV(_maNV) {
+  for (var index = 0; index < dsNV.length; index++) {
+    if (dsNV[index].Ma == _maNV) {
+      return index;
+    }
+  }
 }
